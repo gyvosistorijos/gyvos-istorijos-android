72d46f7317c1a35e986b75fe263fc86470cd503f
Karolis Vycius <karolis@trafi.com>
2016-11-17 21:42:20 +0200

    Show story beginning in geofence notification

--- app/src/main/java/lt/gyvosistorijos/service/GeofenceIntentService.kt
+++ app/src/main/java/lt/gyvosistorijos/service/GeofenceIntentService.kt
@@ -9,10 +9,11 @@ import android.content.Intent
 import android.location.Location
 import android.support.v7.app.NotificationCompat
 import android.text.TextUtils
-import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofencingEvent
 import lt.gyvosistorijos.MainActivity
 import lt.gyvosistorijos.R
+import lt.gyvosistorijos.StoryDb
+import lt.gyvosistorijos.entity.Story
 import lt.gyvosistorijos.location.GeofenceErrorMessages
 import timber.log.Timber
 
@@ -29,6 +30,7 @@ import timber.log.Timber
  * constructor with the name for a worker thread.
  */
 class GeofenceIntentService : IntentService(GeofenceIntentService.TAG) {
+
 
     /**
      * Handles incoming intents.
@@ -51,26 +53,32 @@ class GeofenceIntentService : IntentService(GeofenceIntentService.TAG) {
         val geofenceTransition = geofencingEvent.geofenceTransition
         // Get the geofences that were triggered. A single event can trigger multiple geofences.
         val triggeringGeofences = geofencingEvent.triggeringGeofences
+        val triggeringGeofencesIdsList = triggeringGeofences.map { it.requestId }
 
         // Get the transition details as a String.
         val geofenceTransitionDetails = getGeofenceTransitionDetails(
                 geofenceTransition,
                 triggeredLocation,
-                triggeringGeofences
+                triggeringGeofencesIdsList
         )
 
-        // Send notification and log the transition details.
-        sendNotification()
         Timber.i(geofenceTransitionDetails)
+
+        val triggeredStory = getTriggeredStory(triggeringGeofencesIdsList)
+
+        sendNotification(triggeredStory)
+
+    }
+
+    private fun getTriggeredStory(storyIds: List<String>): Story {
+        return StoryDb.getByIds(storyIds).first()
     }
 
     private fun getGeofenceTransitionDetails(
             transition: Int,
             triggeredLocation: Location?,
-            triggeringGeofences: List<Geofence>): String {
+            triggeringGeofencesIdsList: List<String>): String {
 
-        // Get the Ids of each geofence that was triggered.
-        val triggeringGeofencesIdsList = triggeringGeofences.map { it.requestId }
         val triggeringGeofencesIdsString = TextUtils.join(", ", triggeringGeofencesIdsList)
 
         return "Geofence triggered at $triggeredLocation with transition $transition: $triggeringGeofencesIdsString"
@@ -80,7 +88,7 @@ class GeofenceIntentService : IntentService(GeofenceIntentService.TAG) {
      * Posts a notification in the notification bar when a transition is detected.
      * If the user clicks the notification, control goes to the MainActivity.
      */
-    private fun sendNotification() {
+    private fun sendNotification(story: Story) {
         // Create an explicit content Intent that starts the main Activity.
         val notificationIntent = Intent(applicationContext, MainActivity::class.java)
 
@@ -96,19 +104,19 @@ class GeofenceIntentService : IntentService(GeofenceIntentService.TAG) {
         // Get a PendingIntent containing the entire back stack.
         val notificationPendingIntent = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
 
-        // Get a notification builder that's compatible with platform versions >= 4
         val builder = NotificationCompat.Builder(this)
 
         // Define the notification settings.
         builder.setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(getString(R.string.geofence_notification_text))
-                .setContentIntent(notificationPendingIntent).priority = NotificationCompat.PRIORITY_MIN
+                .setContentText(story.text)
+                .setCategory(NotificationCompat.CATEGORY_RECOMMENDATION)
+                .setContentIntent(notificationPendingIntent).priority = NotificationCompat.PRIORITY_LOW
 
-        // Get an instance of the Notification manager
+        val notification = builder.build()
+
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-
-        // Issue the notification
-        notificationManager.notify(0, builder.build())
+        notificationManager.notify(0, notification)
     }
 
 
